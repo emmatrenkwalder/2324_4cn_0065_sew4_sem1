67ffc29d96395b0e5c66bb61a51661a14da17199
Emma Trenkwalder <0065@htl.rennweg.at>
2023-10-09 15:26:20 +0200

    fertig mit kommentaren und allen versionen

--- ue01/src/secondtry/WordCount.java
+++ ue01/src/secondtry/WordCount.java
@@ -1,9 +1,178 @@
 package secondtry;
+/**
+ * zum Zählen von Wörtern in einem Text,
+ * während HTML-Tags und Sonderzeichen berücksichtigt werden.
+ */
+public class WordCount {
+
+    /**
+     * Diese Methode zählt die Wörter in einem Text, wobei HTML-Tags und Sonderzeichen berücksichtigt werden.
+     *
+     * @param words Der Eingabetext, in dem die Wörter gezählt werden sollen.
+     * @return Die Anzahl der Wörter im Text.
+     */
+    public static int count(String words) {
+        words = onlyOneQuote(words);
+        words = removeBackslashFromQuotes(words);
+        words = removeTextInsideTags(words);
+
+        String newText = words.replaceAll("<[ A-z0-9\\\"=]+>", " ");
+        String filtered = newText.replaceAll("\\s+", " ");
+
+        if (checkIfTagOpen(filtered)) {
+            if (whichOpen(filtered)) {
+                // nach links offen
+                filtered = words.replaceAll("<[ A-z0-9\\\"=>]+>", " ");
+                filtered = newText.replaceAll("\\s+", " ");
+            } else {
+                // nach rechts offen
+                filtered = filtered.substring(0, filtered.indexOf("<"));
+            }
+        }
+
+        for (char c : filtered.toCharArray()) {
+            if (c == '<' || c == '>') {
+                return count(filtered);
+            }
+        }
+
+        // Sonderzeichen check
+        filtered = filtered.replaceAll("[^A-z0-9 ]", " ");
+        filtered = filtered.replaceAll("\\s+", " ");
+
+        String countList[] = filtered.split(" ");
+        if (countList.length > 1) {
+            if (countList[0].equals("")) {
+                return countList.length - 1;
+            } else {
+                return countList.length;
+            }
+        } else if (countList.length == 1 && countList[0] != "") {
+            return countList.length;
+        }
+
+        return 0;
+    }
+
+    /**
+     * Überprüft, ob im Text HTML-Tags offen oder geschlossen sind.
+     *
+     * @param str Der Text, der überprüft werden soll.
+     * @return {@code true}, wenn HTML-Tags offen sind, ansonsten {@code false}.
+     */
+    public static boolean checkIfTagOpen(String str) {
+        int c = 0;
+        str = str.replaceAll("\"([^\"]*)\"", "");
+
+        for (char cha : str.toCharArray()) {
+            if (cha == '<' || cha == '>') {
+                c++;
+            }
+        }
+        return c % 2 != 0;
+    }
+
+    /**
+     * Bestimmt, ob im Text mehr HTML-Tags geöffnet oder geschlossen sind.
+     *
+     * @param str Der Text, der überprüft werden soll.
+     * @return {@code true}, wenn mehr HTML-Tags geöffnet sind, ansonsten {@code false}.
+     */
+    public static boolean whichOpen(String str) {
+        str = str.replaceAll("\"([^\"]*)\"", "");
+        int oc = 0;
+        int cc = 0;
+        for (char cha : str.toCharArray()) {
+            if (cha == '<') {
+                oc++;
+            } else if (cha == '>') {
+                cc++;
+            }
+        }
+        return oc > cc;
+    }
+
+    /**
+     * deleted doppelte Anführungszeichen im Text.
+     *
+     * @param str Der Text der aus dem doppelte Anführungszeichen entfernt werden soll.
+     * @return Der Text ohne doppelte Anführungszeichen.
+     */
+    public static String removeBackslashFromQuotes(String str) {
+        return str.replace("\\\"", "");
+    }
+
+    /**
+     * Entfernt ein einzelnes " im Text, wenn nur eines vorhanden ist.
+     *
+     * @param str Der Text, aus dem ein einzelnes " entfernt werden soll.
+     * @return Der Text ohne das einzelne Anführungszeichen.
+     */
+    public static String onlyOneQuote(String str) {
+        int c = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '"') {
+                c++;
+            }
+        }
+        if (c == 1) {
+            return str.substring(0, str.indexOf("\"")) + str.substring(str.indexOf("\"") + 1);
+        }
+        return str;
+    }
+
+    /**
+     * Entfernt Text innerhalb von HTML-Tags mit den tags
+     *
+     * @param str Der Text, aus dem Text innerhalb von HTML-Tags entfernt werden soll.
+     * @return Der Text ohne den Inhalt der HTML-Tags.
+     */
+    public static String removeTextInsideTags(String str) {
+        int openind = str.indexOf("<");
+        int closeind = str.lastIndexOf(">");
+        int firstg = str.indexOf("\"");
+        int lastg = 0;
+
+        for (int i = firstg + 1; i < str.length(); i++) {
+            if (str.charAt(i) == '"') {
+                lastg = i;
+                break;
+            }
+        }
+
+        String ret = str;
+        if (openind != -1 && closeind != -1 && openind < firstg && lastg < closeind) {
+            ret = str.substring(0, firstg) + str.substring(lastg + 1);
+        }
+        return ret;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*
 /**
  * Diese Klasse enthält eine Methode zum Zählen der Wörter in Texten-> HTML-Tags werden in  speziellen Fällen ignoriet
  */
+
+
+
+
+
+//hat sogar funktioniert
+/*
 public class WordCount {
 
     /**
@@ -12,6 +181,7 @@ public class WordCount {
      * @param s Der Text, in dem die Wörter gezählt werden sollen.
      * @return Die Anzahl der Wörter im Text.
      */
+/*
     public static long count(String s) {
         // Überprüfe, ob der Text einen der speziellen Fälle enthält
         if (s.contains("<img alt=\"<bild keinwort> keinwort") ||
@@ -58,7 +228,7 @@ public class WordCount {
         }
     }
 }
-
+*/
 
 //das hat nicht funktioniert
 

96aec67e15ab0dec17bee657bf66ec23bb61bfd1
Emma Trenkwalder <0065@htl.rennweg.at>
2023-10-23 07:49:26 +0200

    B2 mit test

--- ue03/src/firsttry/csv.java
+++ ue03/src/firsttry/csv.java
@@ -1,30 +1,44 @@
 package firsttry;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 class CSVReader {
     private String line;
     private char delimiter;
+    private char quote;
 
     public CSVReader(String line) {
-        this(line, ',');
+        this(line, ',', '"');
     }
 
-    public CSVReader(String line, char delimiter) {
+    public CSVReader(String line, char delimiter, char quote) {
         this.line = line;
         this.delimiter = delimiter;
+        this.quote = quote;
     }
 
-    public String[] split() {
-        return line.split(String.valueOf(delimiter));
-    }
+    public String[] split() throws IOException {
+        List<String> result = new ArrayList<>();
+        StringBuilder field = new StringBuilder();
+        boolean insideQuotedField = false;
 
-    public static void main(String[] args) {
-        CSVReader csvReader = new CSVReader("emma,17,verzweifelt");
-        String[] data = csvReader.split();
-
-        for (String field : data) {
-            System.out.println(field);
+        for (char c : line.toCharArray()) {
+            if (c == quote) {
+                insideQuotedField = !insideQuotedField;
+            } else if (c == delimiter && !insideQuotedField) {
+                result.add(field.toString());
+                field.setLength(0);
+            } else {
+                field.append(c);
+            }
         }
+
+        result.add(field.toString());
+        if (insideQuotedField) {
+            throw new IOException("Unmatched quote in CSV datei .");
+        }
+        return result.toArray(new String[0]);
     }
 }
--- ue03/src/firsttry/test.java
+++ ue03/src/firsttry/test.java
@@ -1,29 +1,34 @@
 package firsttry;
 
-import org.junit.Test;
-
 import static org.junit.Assert.*;
-
+import org.junit.Test;
+import java.io.IOException;
 
 class CSVReaderTest {
 
     @Test
-    public void testCSVReaderWithDefaultDelimiter() {
-        CSVReader csvReader = new CSVReader("lionel,17,1150");
+    public void testCSVReaderWithQuotes() throws IOException {
+        CSVReader csvReader = new CSVReader("\"John\",\"30\",\"New York\"");
         String[] data = csvReader.split();
 
-        assertEquals("lionel", data[0]);
-        assertEquals("17", data[1]);
-        assertEquals("1150", data[2]);
+        assertEquals("John", data[0]);
+        assertEquals("30", data[1]);
+        assertEquals("New York", data[2]);
     }
 
     @Test
-    public void testCSVReaderWithCustomDelimiter() {
-        CSVReader csvReader = new CSVReader("emma;17;schwadorf", ';');
+    public void testCSVReaderWithCustomDelimiterAndQuotes() throws IOException {
+        CSVReader csvReader = new CSVReader("'Emma' | '17' | 'schwadorf'", '|', '\'');
         String[] data = csvReader.split();
 
         assertEquals("emma", data[0]);
         assertEquals("17", data[1]);
         assertEquals("schwadorf", data[2]);
+    }
+
+    @Test(expected = IOException.class)
+    public void testCSVReaderWithUnmatchedQuote() throws IOException {
+        CSVReader csvReader = new CSVReader("\"lio\",\"700\",\"fünfhaus");
+        csvReader.split(); // Should throw an IOException
     }
 }
